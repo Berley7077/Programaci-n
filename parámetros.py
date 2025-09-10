22cc7d13f4201c3360ab635b07dfa3384899898b
Berley Guamuch <maldonadoberly25@gmail.com>
2025-09-10 08:50:03 -0600

    parámetros.py

--- parámetros.py
+++ parámetros.py
@@ -1,1 +1,6 @@
-
+#Parámetros por valor y referencia 
+#definimos una función que recibe un num
+def por_valor (x):
+    #Dentro de la f, sumar un 6 al valor recibido
+    x+= 6
+    print("Dentro por_valor:", x)
