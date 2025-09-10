6b5d95999aacf210eddf0cfcebc56f3a3e28043c
Berley Guamuch <maldonadoberly25@gmail.com>
2025-09-10 09:00:52 -0600

    parámetros.py

--- parámetros.py
+++ parámetros.py
@@ -4,3 +4,16 @@ def por_valor (x):
     #Dentro de la f, sumar un 6 al valor recibido
     x+= 6
     print("Dentro por_valor:", x)
+    #Definimos una función por medio de lista 
+def por_referencia (lista):
+    #Se agrega el número 99 a la lista.
+    lista.append (99)
+    print("Dentro por_referencia", lista)
+#Ejemplo con número entero
+n->5
+por_valor (n)
+print("fuera n=",)
+#Ejemplo con lista 
+num =[1,2]
+por_referencia (num)
+print("fuera num=", num)
